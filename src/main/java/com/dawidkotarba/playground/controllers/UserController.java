cd77620293e23629f1b6650fc1532a3166e6ebc7
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-01-05 12:41:24 +0100

    add user controller

--- src/main/java/com/dawidkotarba/playground/controllers/UserController.java
+++ src/main/java/com/dawidkotarba/playground/controllers/UserController.java
@@ -1,7 +1,8 @@
 package com.dawidkotarba.playground.controllers;
 
-import com.dawidkotarba.playground.integration.dto.CountryDto;
-import com.dawidkotarba.playground.service.CountryService;
+import com.dawidkotarba.playground.integration.dto.UserInDto;
+import com.dawidkotarba.playground.integration.dto.UserOutDto;
+import com.dawidkotarba.playground.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -9,7 +10,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -17,32 +17,31 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Created by Dawid Kotarba on 12.11.2015.
+ * Created by Dawid Kotarba on 15.11.2015.
  */
-
-@Path("/countries")
+@Path("/users")
 @RequestScoped
-public class CountryController {
+public class UserController {
 
     @Inject
-    private CountryService countryService;
+    private UserService userService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CountryDto> all() {
-        return countryService.getAll();
+    public List<UserOutDto> getAll() {
+        return userService.getAll();
     }
-    
+
     @GET
     @Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CountryDto> getByName(@PathParam("name") String name) {
-        return countryService.getByName(name);
+    public List<UserOutDto> getByName(@PathParam("name") String name) {
+        return userService.getByName(name);
     }
 
     @PUT
     @Consumes(MediaType.APPLICATION_XML)
-    public void add(@Valid CountryDto countryDto) {
-        countryService.add(countryDto);
+    public void add(@Valid UserInDto userInDto) {
+        userService.add(userInDto);
     }
 }
